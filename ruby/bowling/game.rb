24589651447b9bccb4fd5b12ea044bbdc71b3234
Paul <paulgregorybyrne@gmail.com>
2017-04-07 15:13:55 +0200

    Refactoring

--- ruby/bowling/game.rb
+++ ruby/bowling/game.rb
@@ -1,41 +1,47 @@
 # Calculate the score for a game of bowling
 class Game
   attr_reader :score
+
   def initialize
     @score = 0
-    @recent_rolls = []
+    @recent_rolls = [0, 0, 0]
     @frame_count = 1
     @frame_throw_count = 0
+    @game_over = false
   end
 
-
   def roll(pin)
-    if @frame_count == 12
-      return
-    end
-    if @frame_count == 11
+    return if @game_over
+    if one_roll_bonus?
       @score += pin
-      @frame_count == 12
+      @game_over = true
       return
     end
     @frame_throw_count += 1
-    if pin == 10
-      @frame_count += 1
-      @frame_throw_count = 0
-    elsif @frame_throw_count == 2
+    if end_of_frame?(pin)
       @frame_count += 1
       @frame_throw_count = 0
     end
     @score += pin
-    @recent_rolls.push(pin)
-    if @recent_rolls.size == 3
-      add_outstanding
-      @recent_rolls.shift
-    end
+    update_rolls(pin)
+    add_outstanding
     @score
   end
 
   private
+
+  def one_roll_bonus?
+    @frame_count == 11
+  end
+
+  def end_of_frame?(pin)
+    pin == 10 || @frame_throw_count == 2
+  end
+
+  def update_rolls(pin)
+    @recent_rolls.push(pin)
+    @recent_rolls.shift
+  end
 
   def add_outstanding
     if had_strike?
