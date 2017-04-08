1fb592aa04a17363dd3f3bd689484c8327ac0c4a
Paul <paulgregorybyrne@gmail.com>
2017-04-08 20:12:56 +0200

    Redesign Bowling implementation

--- ruby/bowling/game.rb
+++ ruby/bowling/game.rb
@@ -1,82 +1,56 @@
 # Calculate the score for a game of bowling
+require 'pry'
 class Game
-  attr_reader :score
-
+  attr_accessor :score
   def initialize
     @score = 0
-    @recent_rolls = [0, 0, 0]
-    @frame_count = 1
-    @frame_throw_count = 0
-    @game_over = false
+    @doubles = 0
+    @frame = 1
+    @frames = []
+    @current_frame = []
   end
 
   def roll(pin)
-    return if @game_over
-    if one_roll_bonus?
-      @score += pin
-      @game_over = true
-      return
-    end
-    @frame_throw_count += 1
-    if end_of_frame?(pin)
-      @frame_count += 1
-      @frame_throw_count = 0
-    end
-    @score += pin
-    update_rolls(pin)
-    add_outstanding
-    @score
+    update_frame(pin)
+    update_score(pin)
+    update_doubles(pin)
+    end_frame if is_strike?(pin) || is_spare?(pin) || is_open?(pin)
   end
 
   private
 
-  def one_roll_bonus?
-    @frame_count == 11
+  def update_frame(pin)
+    @current_frame.push(pin)
   end
 
-  def end_of_frame?(pin)
-    pin == 10 || @frame_throw_count == 2
+  def update_score(pin)
+    @score += pin
+    return unless @doubles > 0
+    @score += pin
+    @doubles -= 1
   end
 
-  def update_rolls(pin)
-    @recent_rolls.push(pin)
-    @recent_rolls.shift
+  def update_doubles(pin)
+    @doubles += 2 if is_strike?(pin)
+    @doubles += 1 if is_spare?(pin)
   end
 
-  def add_outstanding
-    if had_strike?
-      calculate_strike
-    end
-    if had_spare?
-      calculate_spare
-    end  
+  def end_frame
+    @frames.push(@current_frame)
+    @current_frame = []
+    return unless @frame < 11
+    @frame += 1
   end
 
-  def had_strike?
-    second_last_roll == 10
+  def is_strike?(pin)
+    pin == 10
   end
 
-  def had_spare?
-    (last_roll + second_last_roll) == 10
+  def is_spare?(pin)
+    (@current_frame.size == 2) && (pin + @current_frame[0] == 10)
   end
 
-  def calculate_strike
-    @score += (second_last_roll + last_roll)
-  end
-
-  def calculate_spare
-    @score += current_roll
-  end
-
-  def current_roll
-    @recent_rolls[2]
-  end
-
-  def last_roll
-    @recent_rolls[1]
-  end
-
-  def second_last_roll
-    @recent_rolls[0]
+  def is_open?(pin)
+    (@current_frame.size == 2) && (pin + @current_frame[0] < 10)
   end
 end
