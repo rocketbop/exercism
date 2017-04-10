1cb8c38e2cc20f25ac5b584218e6ee5ba0a70e9c
Paul <paulgregorybyrne@gmail.com>
2017-04-10 23:24:37 +0200

    Add BowlingError class

--- ruby/bowling/bowling_test.rb
+++ ruby/bowling/bowling_test.rb
@@ -39,67 +39,67 @@ class BowlingTest < Minitest::Test
   end
 
   def test_consecutive_spares_each_get_a_one_roll_bonus
-    skip
+    # skip
     roll([5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 31, @game.score
   end
 
   def test_a_spare_in_the_last_frame_gets_a_one_roll_bonus_that_is_counted_once
-    skip
+    # skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7])
     assert_equal 17, @game.score
   end
 
   def test_a_strike_earns_ten_points_in_a_frame_with_a_single_roll
-    skip
+    # skip
     roll([10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 10, @game.score
   end
 
   def test_points_scored_in_the_two_rolls_after_a_strike_are_counted_twice_as_a_bonus
-    skip
+    # skip
     roll([10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 26, @game.score
   end
 
   def test_consecutive_strikes_each_get_the_two_roll_bonus
-    skip
+    # skip
     roll([10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal 81, @game.score
   end
 
   def test_a_strike_in_the_last_frame_gets_a_two_roll_bonus_that_is_counted_once
-    skip
+    # skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1])
     assert_equal 18, @game.score
   end
 
   def test_rolling_a_spare_with_the_two_roll_bonus_does_not_get_a_bonus_roll
-    skip
+    # skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3])
     assert_equal 20, @game.score
   end
 
   def test_strikes_with_the_two_roll_bonus_do_not_get_bonus_rolls
-    skip
+    # skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])
     assert_equal 30, @game.score
   end
 
   def test_a_strike_with_the_one_roll_bonus_after_a_spare_in_the_last_frame_does_not_get_a_bonus
-    skip
+    # skip
     roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10])
     assert_equal 20, @game.score
   end
 
   def test_all_strikes_is_a_perfect_game
-    skip
+    # skip
     roll([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
     assert_equal 300, @game.score
   end
 
   def test_rolls_can_not_score_negative_points
-    skip
+    # skip
     assert_raises Game::BowlingError do
       roll([-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
       @game.score
--- ruby/bowling/game_2.rb
+++ ruby/bowling/game_2.rb
@@ -1,32 +1,61 @@
 require 'pry'
+
+#  Game class rolls and scores 
 class Game
   def initialize
     @frames = [Frame.new]
   end
 
   def roll(pin)
-    current_frame.save_throw(pin)
-    return unless current_frame.strike? || current_frame.throw == 2
+    raise BowlingError.new("BowlingError") if pin < 0
+    current_frame.update_throw(pin)
+    return unless current_frame.strike? || current_frame.complete?
     @frames.push(Frame.new)
   end
 
   def score
-    @frames.map(&:score).sum
+    total = 0
+    @frames.each_with_index do |frame, index|
+      break if index > 9
+      total += frame.score
+      if frame.complete? && frame.spare?
+        total += @frames[index + 1].throws.first
+      elsif frame.strike?
+        total += score_strike(index)
+      end
+    end
+    total
   end
 
- private
+  private
 
   def current_frame
     @frames.last
   end
+
+  def score_strike(strike_index)
+    total = 0
+    total += @frames[strike_index + 1].throws[0]
+    total += if @frames[strike_index + 1].throws.size == 2
+               @frames[strike_index + 1].throws[1]
+             else
+               @frames[strike_index + 2].throws[0]
+             end
+    total
+  end
+end
+
+class BowlingError < StandardError
 end
 
 class Frame
+  attr_accessor :throws
+
   def initialize
     @throws = []
   end
 
-  def save_throw(pin)
+  def update_throw(pin)
     @throws.push(pin)
   end
 
@@ -34,11 +63,15 @@ class Frame
     @throws.sum
   end
 
-  def throw
-    @throws.size + 1
+  def complete?
+    @throws.size == 2 || strike?
   end
 
   def strike?
     @throws.first == 10
+  end
+
+  def spare?
+    @throws.size == 2 && score == 10
   end
 end
