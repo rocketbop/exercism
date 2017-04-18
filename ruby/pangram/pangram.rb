d8332bd2053099d0d32cb2db00c5c8aace34d643
Paul <paulgregorybyrne@gmail.com>
2017-04-18 18:23:35 +0200

    Add Pangram

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,14 @@
+class Pangram
+  def self.pangram?(phrase)
+    alphabet = ('A'..'Z').to_a
+    phrase.upcase.each_char do |char|
+      alphabet.delete_if { |letter| letter == char }
+      return true if alphabet.empty?
+    end
+    alphabet.empty?
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
