63e428b1c975b6b50fb4c3d0ffedb5abfed69c84
Paul <paulgregorybyrne@gmail.com>
2017-04-18 14:43:51 +0200

    Use << for performance reasons

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,12 +1,11 @@
 class Raindrops
   def self.convert(number)
-    result = ""
-    result += "Pling" if factor_of?(factor: number, product: 3)
-    result += "Plang" if factor_of?(factor: number, product: 5)
-    result += "Plong" if factor_of?(factor: number, product: 7)
+    result = ''
+    result << "Pling" if factor_of?(factor: number, product: 3)
+    result << "Plang" if factor_of?(factor: number, product: 5)
+    result << "Plong" if factor_of?(factor: number, product: 7)
     result.empty? ? number.to_s : result
   end
-
 
   def self.factor_of?(factor:, product:)
     (factor % product).zero?
