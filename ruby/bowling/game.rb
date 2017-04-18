e3de9b7d891871b3a519be9e5aa8a8329b22de08
Paul <paulgregorybyrne@gmail.com>
2017-04-18 13:16:07 +0200

    Include all classes in one file

--- ruby/bowling/game.rb
+++ ruby/bowling/game.rb
@@ -1,5 +1,5 @@
-require_relative "frame"
-require_relative "bowling_error"
+# require_relative "frame"
+# require_relative "bowling_error"
 
 #  Game class rolls and scores
 class Game
@@ -73,6 +73,56 @@ class Game
   end
 end
 
+class Frame
+  attr_accessor :throws, :bonus_frame
+
+  def initialize(bonus_frame = false)
+    @throws = []
+    @bonus_frame = bonus_frame
+  end
+
+  def update_throw(pin)
+    @throws.push(pin)
+    raise Game::BowlingError.new("BowlingError") if invalid?
+  end
+
+  def score
+    @throws.sum
+  end
+
+  def complete?
+    unless @bonus_frame
+      @throws.size == 2 || strike?
+    else
+      false
+    end
+  end
+
+  def strike?
+    @throws.first == 10
+  end
+
+  def spare?
+    @throws.size == 2 && score == 10
+  end
+
+  private
+
+  def invalid?
+    if !@bonus_frame
+      @throws.sum > 10
+    elsif @throws[0] < 10
+      @throws[0] + @throws[1].to_i > 10
+    end
+  end
+end
+
+
 module BookKeeping
   VERSION = 3
+end
+
+class Game
+  class BowlingError < StandardError
+  end
 end
