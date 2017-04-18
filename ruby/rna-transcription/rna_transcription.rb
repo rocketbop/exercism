ea2215445adc6f84d01c8b2a03a16a07cdcdd42e
Paul <paulgregorybyrne@gmail.com>
2017-04-18 14:04:42 +0200

    RNA Transcription complete

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,5 +1,24 @@
+require "pry"
+
 class Complement
-  def self.of_dna(strand)
-    "G"
+  def self.map
+    {
+      "G" => "C",
+      "C" => "G",
+      "T" => "A",
+      "A" => "U"
+    }
   end
+
+  def self.of_dna(sequence)
+    sequence.each_char.map do |char|
+      compliment = map[char]
+      return "" if compliment.nil?
+      compliment
+    end.join
+  end
+end
+
+module BookKeeping
+  VERSION = 4
 end
--- ruby/rna-transcription/rna_transcription_test.rb
+++ ruby/rna-transcription/rna_transcription_test.rb
@@ -13,37 +13,37 @@ class ComplementTest < Minitest::Test
   end
 
   def test_rna_complement_of_guanine_is_cytosine
-    skip
+    # skip
     assert_equal 'C', Complement.of_dna('G')
   end
 
   def test_rna_complement_of_thymine_is_adenine
-    skip
+    # skip
     assert_equal 'A', Complement.of_dna('T')
   end
 
   def test_rna_complement_of_adenine_is_uracil
-    skip
+    # skip
     assert_equal 'U', Complement.of_dna('A')
   end
 
   def test_rna_complement
-    skip
+    # skip
     assert_equal 'UGCACCAGAAUU', Complement.of_dna('ACGTGGTCTTAA')
   end
 
   def test_dna_correctly_handles_invalid_input
-    skip
+    # skip
     assert_equal '', Complement.of_dna('U')
   end
 
   def test_dna_correctly_handles_completely_invalid_input
-    skip
+    # skip
     assert_equal '', Complement.of_dna('XXX')
   end
 
   def test_dna_correctly_handles_partially_invalid_input
-    skip
+    # skip
     assert_equal '', Complement.of_dna('ACGTXXXCTTAA')
   end
 
@@ -64,7 +64,7 @@ class ComplementTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+    # skip
     assert_equal 4, BookKeeping::VERSION
   end
 end
